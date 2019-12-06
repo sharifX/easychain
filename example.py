0bda88571f5fea3d3febc8120cb5937a131e587b
Sharif Islam <50382675+sharifX@users.noreply.github.com>
2019-12-06 12:26:57 +0100

    Update example.py

--- example.py
+++ example.py
@@ -49,3 +49,17 @@ pickle.dump(tampered, open('chain.p', 'wb'))
 print("Deserializing tampered chain and validating...")
 chain3 = pickle.load(open('chain.p', 'rb'))
 chain3.validate()       # EARTH-SHATTERING KABOOM
+
+
+>>> from blockchain import Message, Block, Blockchain
+>>> import pickle
+>>> B1 = Block()
+>>> B1.add_message(Message("Q6780787 collected S-G-3969"))
+>>> B1.add_message(Message("S-G-2969 deposited at NHMUK"))
+>>> B1.add_message(Message("S-G-2969 aggregated", "Q6780787", "NHMUK"))
+>>> block1 = Block()
+>>> B1
+Block<hash: None, prev_hash: None, messages: 3, time: None>
+>>> B1.__dict__
+{'timestamp': None, 'messages': [Message<hash: 27b93f5bb183eea89e63ad4d98bfa13c9083d6023b034ac309e0c05e666c80a4, prev_hash: None, sender: None, receiver: None, data: Q6780787 collected S-G-39>, Message<hash: c1303ee1ce75b045380de19b61dd78d2b583f07bea580f0bff8b1cced84a48fa, prev_hash: 27b93f5bb183eea89e63ad4d98bfa13c9083d6023b034ac309e0c05e666c80a4, sender: None, receiver: None, data: S-G-2969 deposited at NHM>, Message<hash: 69d8c326efa8796360da4b28bd29367a9de96cfe1e8504660a7e7f75d3228d76, prev_hash: c1303ee1ce75b045380de19b61dd78d2b583f07bea580f0bff8b1cced84a48fa, sender: Q6780787, receiver: NHMUK, data: S-G-2969 aggregated>], 'hash': None, 'prev_hash': None}
+>>> 
